115f23691f68de691f1ffe359a8f7f2b8e6dece3
Jérôme Musqua <jmusqua@wiuz.fr>
2022-03-09 10:25:59 +0100

    Initial commit

---
+++ ConsoleWiuz/Cultures/TestCulture.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------
+//  This source file is the confidential property and copyright of WIUZ.
+//  Reproduction or transmission in whole or in part, in any form or
+//  by any means, electronic, mechanical or otherwise, is prohibited
+//  without the prior written consent of the copyright owner.
+//  <copyright file="TestCulture.cs" company="WIUZ">
+//     Copyright (C) WIUZ.  All rights reserved. 2016 - 2022
+//  </copyright>
+//  History:
+//   2022/03/09 at 07:52: Jerôme MUSQUA aka JerômeMUSQUA.
+// ---------------------------------------------------------------------------
+
+using Xunit;
+
+namespace ConsoleWiuz;
+
+public class TestCulture
+{
+    [Fact]
+    public void CultureNull()
+    {
+        //6
+    }
+
+    [Fact]
+    public void CultureBle()
+    {
+        //188, 5
+    }
+}
